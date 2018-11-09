1fff82c8c6c96f7479f73206a5ff805f19b27d3a
Jean Chquimarca <jeanchuquimarca12@gmail.com>
2018-11-09 10:18:10 -0500

    agregando cambios

---
+++ ejercicio1/mipaquete/Empleado.py
@@ -0,0 +1,29 @@
+def __init__(self):
+    self nombre =" "
+    self apellido = " "
+    self cedula = " "
+    self comision_fija = 0
+
+def agregar_comision_fija(self, apellidos):
+    self.comision_fija = apellidos
+def obtener_comision_fija(self)
+    return self.comision_fija
+
+def agregar_cedula(self, cedulas):
+    self.cedula = cedulas
+def obtener_cedula(self)
+    return self.cedula
+
+def agregar_apellido(self, comisiones):
+    self.apellido = comisiones
+def obtener_apellido(self)
+    return self.apellido
+
+def agregar_nombre(self, nombres):
+    self.nombre = nombres
+def obtener_nombre(self)
+    return self.nombre
+
+def presentar_datos(self):
+    cadena = "informacion de %s %s %s\n\tcedula%s"%(self.obtener_nombre),(self.obtener_apellido), (self.obtenercedula)
+    return cadena
---
+++ ejercicio1/mipaquete/EmpleadoFijo.py
@@ -0,0 +1,15 @@
+def __init__(self):
+    self sueldo_fijo
+	self descuento_seguro
+
+def agregar_sueldo_fijo(self, sueldos):
+    self.sueldo_fijo=sueldos
+def obtener_sueldo_fijo(self):
+    return self.sueldo_fijo
+
+def agregar_descuento_seguro(self, descuentos):
+    self.descuento_seguro=descuentos
+def obtener_descuento_seguro(self):
+    return self.descuento_seguro
+
+def calcular_sueldo_final():
